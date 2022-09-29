3341e3912895ab3ad6027501b1b08388957db707
aigulabdullina <aigulabdullina@gmail.com>
2022-09-29 12:41:56 +0200

    Add HW 3 first level

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
-    public static void main(String [] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        String IStudyBasicJavaStr = "I study Basic Java!";
+        System.out.println(IStudyBasicJavaStr.charAt(18));
+        System.out.println(IStudyBasicJavaStr.contains("Java"));
+        System.out.println(IStudyBasicJavaStr.replace("a","o" ));
+        System.out.println(IStudyBasicJavaStr.toUpperCase());
+        System.out.println(IStudyBasicJavaStr.toLowerCase());
+        System.out.println(IStudyBasicJavaStr.substring(0,13));
+
+
+        double percent = (200.0 / 470) * 100;
+        System.out.println(percent);
+
+        char g = 'g';
+        int in = Character.getNumericValue(g);
+        System.out.println(in);
+
     }
 }
