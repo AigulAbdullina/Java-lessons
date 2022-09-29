41c295332251441d75ce653d82ef3e0e3b02b246
aigulabdullina <aigulabdullina@gmail.com>
2022-09-29 18:01:31 +0200

    Add HW 3 SecondLvl

---
+++ src/HW3SecondLvl.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class HW3SecondLvl {
+    public static <string> void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name);
+        System.out.println("Ваше имя начинается с буквы " + name.charAt(0));
+
+
+        double param = 59.976;
+        int paramRound = (int) Math.round(param);
+        System.out.println(paramRound);
+
+
+        int val1 = 475;
+        short val2 = (short)val1;
+        System.out.println(val2);
+
+
+    }
+}
--- src/HW3firstLvl.java
+++ src/HW3firstLvl.java
@@ -1,13 +1,14 @@
-public class Main {
+public class HW3firstLvl {
     public static void main(String[] args)
     {
         String IStudyBasicJavaStr = "I study Basic Java!";
-        System.out.println(IStudyBasicJavaStr.charAt(18));
+        System.out.println(IStudyBasicJavaStr.charAt(IStudyBasicJavaStr.length()-1));
         System.out.println(IStudyBasicJavaStr.contains("Java"));
         System.out.println(IStudyBasicJavaStr.replace("a","o" ));
         System.out.println(IStudyBasicJavaStr.toUpperCase());
         System.out.println(IStudyBasicJavaStr.toLowerCase());
-        System.out.println(IStudyBasicJavaStr.substring(0,13));
+        System.out.println(IStudyBasicJavaStr.substring(0,IStudyBasicJavaStr.indexOf("Java")));
+
 
 
         double percent = (200.0 / 470) * 100;
